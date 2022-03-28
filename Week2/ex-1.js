e5665eb843256b20b72c77425a9c7203085b0f10
AliDemircix <alidemircix@gmail.com>
2022-03-28 17:46:38 +0200

    Ex-1 Finished

---
+++ Week2/ex-1.js
@@ -0,0 +1,49 @@
+const util = require('util');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  port: 3306,
+});
+
+const execQuery = util.promisify(connection.query.bind(connection));
+
+async function seedDatabase() {
+  const dropDatabase = `DROP DATABASE IF EXISTS ex1;`;
+  const createDatabase = `CREATE DATABASE ex1;`;
+  const useDatabase = `USE ex1;`;
+
+  const createTableAuthors = `
+  CREATE TABLE authors (
+    author_no INT,
+    author_name VARCHAR(50),
+    university VARCHAR(300),
+    date_of_birth DATE,
+    h_index SMALLINT,
+    gender ENUM ('Male', 'Female'),
+    PRIMARY KEY (author_no)
+  );`;
+
+  const addColumnMentor = `
+  ALTER TABLE authors
+  ADD COLUMN mentor INT,
+  ADD CONSTRAINT FOREIGN KEY(mentor) REFERENCES authors(author_no);`;
+
+  connection.connect();
+
+  try {
+    await execQuery(dropDatabase);
+    await execQuery(createDatabase);
+    await execQuery(useDatabase);
+    await execQuery(createTableAuthors);
+    await execQuery(addColumnMentor);
+  } catch (err) {
+    console.error(err.message);
+  }
+
+  connection.end();
+}
+
+seedDatabase();
